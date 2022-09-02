5253b801e99e8cacf60a47e53d1a559085502436
Tushar Ulhare <tusharulhare1999@gmail.com>
2022-09-02 11:47:35 +0530

    Second time commit

--- msg.java
+++ msg.java
@@ -1,5 +1,5 @@
 public class msg{
      public static void main(String[] args) {
-        System.out.println("This is msg");
+        System.out.println("This is message is to be print");
     }
 }
---
+++ multiply.java
@@ -0,0 +1,5 @@
+public class multiply {
+    public static void main(String[] args) {
+        System.out.println("This will be used for Multiplication purpose");
+    }
+}
